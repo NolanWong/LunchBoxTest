c71663d9564558bf2ec0ee0d5d42a1ea5920d8e7
ImperialOfficer <nolancwong@gmail.com>
2025-09-23 12:52:28 -0500

    eafuigaugectyrvinhtbuuyngcb74ynri7y3nxruh

---
+++ downloadDrive.js
@@ -0,0 +1,25 @@
+import {GoogleAuth} from 'google-auth-library';
+import {google} from 'googleapis';
+const button=document.getElementById("DWNLDBTTN");
+/**
+ * Downloads a file from Google Drive.
+ * @param {string} fileId The ID of the file to download.
+ * @return {Promise<number>} The status of the download.
+ */
+async function downloadFile() {
+  // Authenticate with Google and get an authorized client.
+  // TODO (developer): Use an appropriate auth mechanism for your app.
+  const auth = new GoogleAuth({
+    scopes: 'https://www.googleapis.com/auth/drive',
+  });
+
+  // Create a new Drive API client (v3).
+  const service = google.drive({version: 'v3', auth});
+
+  // Download the file.
+  const file = await service.files.get("1tbkIdEZEzGHwAh6FviA-WMZFBTjzcyXlk3KZSlNJocY");
+
+  // Print the status of the download.
+  console.log(file.status);
+  return file.status;
+}
