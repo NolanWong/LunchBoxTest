d8415d68022cc9a176ba42349c4777a27258d23f
ImperialOfficer <nolancwong@gmail.com>
2025-10-02 12:34:51 -0500

    ihwufgk

--- downloadDrive.js
+++ downloadDrive.js
@@ -1,24 +1,10 @@
 import {GoogleAuth} from 'google-auth-library';
 import {google} from 'googleapis';
-
+const button=document.getElementById("DWNLDBTTN");
 /**
  * Downloads a file from Google Drive.
- * @param {string} fileId The ID of the file to download.
- * @return {Promise<number>} The status of the download.
  */
-
- function getLatestFileId(){
-
-   var folder = DriveApp.getFoldersByName("LunchBoxDownload");
-   var files = folder.next().getFiles();
-   var lastFileId = files.next().getId();
-
-
-   return lastFileId.toString();
-
-}
-
-async function downloadFile(getLatestFileId) {
+async function downloadFile() {
   // Authenticate with Google and get an authorized client.
   // TODO (developer): Use an appropriate auth mechanism for your app.
   const auth = new GoogleAuth({
@@ -29,12 +15,10 @@ async function downloadFile(getLatestFileId) {
   const service = google.drive({version: 'v3', auth});
 
   // Download the file.
-  const file = await service.files.get({
-    fileId,
-    alt: 'media',
-  });
+  const file = await service.files.get("1tbkIdEZEzGHwAh6FviA-WMZFBTjzcyXlk3KZSlNJocY");
 
   // Print the status of the download.
   console.log(file.status);
   return file.status;
 }
+button.addEventListener("click",downloadFile)
--- push.js
+++ push.js
@@ -17,7 +17,6 @@
                 //handle form submission
                 function submitForm(event) {
                     //email to it's Google Sheet
-                    // 2025- This was made by the last guys. I don't know what it does and The entire program breaks when removed.
                     // const scriptURL = 'https://script.google.com/macros/s/AKfycbyszkeMxNOSgKDwxueAc7aiZcY8ZrvlJlQUnHcWkt63zymvxipmiq-pefW7E3aM1vKa/exec'
                     // const form = document.forms['submit-to-google-sheet']
                     // console.log("submit to google sheet btn triggered")
@@ -30,7 +29,7 @@
                     //     .catch(error => console.error('Error!', error.message));
                     // })
                     
-                    console.log("form submitting")
+                    // console.log("form submitting")
                     //event.preventDefault();
                     const email = document.getElementById('emailINPUT').value;
                     const password = document.getElementById('passwordINPUT').value;
@@ -142,12 +141,5 @@
                     
                     
                 }
-                // function showPass(){
-                //     if(document.getElementById("passwordINPUT").type == "password"){
-                //         document.getElementById("passwordINPUT").type == "text"
-                //     }
-                // }
-                // const showPassBTN = document.getElementById("showPassBTN")
                 const submitBTN = document.getElementById("submitBTN");
-                // showPassBTN.addEventListener("click", showPass)
                 submitBTN.addEventListener("click", submitForm);
