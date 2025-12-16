55563346955e9e9ba252f004bea9ed8b36be52f0
ImperialOfficer <nolancwong@gmail.com>
2025-12-16 13:37:39 -0600

    gtefygeygvi

--- push.js
+++ push.js
@@ -80,8 +80,8 @@ const auth = getAuth();
         const dessertINPUT1 =   document.getElementById('dessertINPUT1').value;
 
         //gets date input 1 and adds two days.
-        const dateINPUT3 =      new Date(document.getElementById('dateINPUT1').value);
-        dateINPUT3.setDate(dateINPUT3.getDate() + 2).toLocaleString('en-US', {timeZone: 'UTC'});
+        const tempInput =      new Date(document.getElementById('dateINPUT1').value);
+        const dateINPUT3 = tempInput.setDate(tempInput.getDate() + 2).toLocaleString('en-US', {timeZone: 'UTC'});
         
         const entreeINPUT3 =    document.getElementById('entreeINPUT3').value;
         const soupINPUT3 =      document.getElementById('soupINPUT3').value;
